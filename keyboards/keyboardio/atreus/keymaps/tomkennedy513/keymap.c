dd44ad5650b929097a3002dcf73457c6ab2bef49
Tom Kennedy <kennedy513@gmail.com>
2024-10-15 19:08:47 -0400

    add hjkl layer
    Signed-off-by: Tom Kennedy <kennedy513@gmail.com>

--- keyboards/keyboardio/atreus/keymaps/tomkennedy513/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tomkennedy513/keymap.c
@@ -10,6 +10,7 @@ enum layer_names {
     _SYM,
     _NUM,
     _FN,
+    _HJKL,
 };
 
 enum custom_keycodes {
@@ -21,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,                                 KC_Y,          KC_U,        KC_I,        KC_O,        CTRL_SPACE,
         SFT_T(KC_A), CTL_T(KC_S), ALT_T(KC_D), CMD_T(KC_F), KC_G,                                 KC_H,          CMD_T(KC_J), ALT_T(KC_K), CTL_T(KC_L), SFT_T(KC_P),
         KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,    OSM(MOD_MEH), OSM(MOD_HYPR), KC_N,          KC_M,        KC_COMM,     KC_DOT,      KC_ENT,
-        KC_LSFT,     KC_LCTL,     KC_LALT,     KC_LCMD,     KC_BSPC, OSL(_SYM),    OSL(_NUM),     SFT_T(KC_SPC), KC_TAB,      KC_ESC,      MO(_FN),     KC_NO),
+        KC_LSFT,     KC_LCTL,     KC_LALT,     KC_LCMD,     KC_BSPC, OSL(_SYM),    OSL(_NUM),     SFT_T(KC_SPC), KC_TAB,      KC_ESC,      MO(_FN),     TO(_HJKL)),
 
     [_SYM] = LAYOUT(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_PLUS, KC_EQL,  KC_QUES, KC_PIPE, KC_BSLS,
@@ -32,14 +33,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT(
         KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                        LCTL(LSFT(KC_TAB)), KC_PGDN, KC_PGUP, LCTL(KC_TAB), KC_NO,
         KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,                        KC_LEFT,            KC_DOWN, KC_UP,   KC_RGHT,      KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   TO(_MAIN), KC_TRNS,  KC_TRNS, KC_NO,              KC_NO,   KC_COMM, KC_DOT,       KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   TO(_MAIN), KC_TRNS,  KC_TRNS, KC_NO,              KC_NO,   KC_COMM, KC_DOT,        KC_NO,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   TO(_SYM), KC_NO,   KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS),
 
     [_FN] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                        KC_NO,   KC_LCMD, KC_LALT, KC_LCTL, KC_LSFT,
         KC_F11,  KC_F12,  KC_NO,   KC_NO,   TO(_MAIN), KC_TRNS,  KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   TO(_SYM), TO(_NUM), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   TO(_SYM), TO(_NUM), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+
+    [_HJKL] = LAYOUT(
+        KC_Q,        KC_W,        KC_F,        KC_P,        KC_G,                                 KC_H,          KC_J,        KC_K,        KC_L,        CTRL_SPACE,
+        SFT_T(KC_A), CTL_T(KC_S), ALT_T(KC_R), CMD_T(KC_T), KC_D,                                 KC_M,          CMD_T(KC_N), ALT_T(KC_E), CTL_T(KC_I), SFT_T(KC_O),
+        KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,    OSM(MOD_MEH), OSM(MOD_HYPR), KC_Y,          KC_U,        KC_COMM,     KC_DOT,      KC_ENT,
+        KC_LSFT,     KC_LCTL,     KC_LALT,     KC_LCMD,     KC_BSPC, OSL(_SYM),    OSL(_NUM),     SFT_T(KC_SPC), KC_TAB,      KC_ESC,      MO(_FN),     TO(_MAIN)),
 };
 
 const uint16_t PROGMEM esc_combo[] = {OSL(_SYM), OSL(_NUM), COMBO_END};
